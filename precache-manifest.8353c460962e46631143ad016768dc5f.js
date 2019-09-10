801970162a5aee08feecbbb63efb7c5e29307bc4
Roman Gruba <roman.gruba@gmail.com>
2019-09-10 16:57:29 +0300

    Updates

---
+++ precache-manifest.8353c460962e46631143ad016768dc5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7374e75b5ba875786be9d89a680d25b",
+    "url": "/goit-react-hw-04-pet-store/index.html"
+  },
+  {
+    "revision": "ea27d083621c4b663089",
+    "url": "/goit-react-hw-04-pet-store/static/css/main.146a48ec.chunk.css"
+  },
+  {
+    "revision": "bba9f899616a932b64ab",
+    "url": "/goit-react-hw-04-pet-store/static/js/2.93b21547.chunk.js"
+  },
+  {
+    "revision": "ea27d083621c4b663089",
+    "url": "/goit-react-hw-04-pet-store/static/js/main.bdf9c0e1.chunk.js"
+  },
+  {
+    "revision": "6566220e94515017cee7",
+    "url": "/goit-react-hw-04-pet-store/static/js/runtime~main.b0d8e902.js"
+  }
+]);
